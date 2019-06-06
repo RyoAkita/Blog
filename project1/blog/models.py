e3756bdfa510b2615728804a11bb8dd31aae2e41
アキタリョウ <ryotennis0503@gmail.com>
2019-06-06 23:56:21 +0900

    create model

--- project1/blog/models.py
+++ project1/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    #Define category type
+    name = models.CharField('カテゴリ名', max_length = 50)
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__ (self):
+        return self.name
+
+
+
+
+
+
+class Post(models.Model):
+    #Define post data contents
+
+    title = models.CharField('タイトル', max_length = 50)
+    text = models.TextField('本文')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__ (self):
+        return self.title
